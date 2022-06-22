71125cca33133d575dd7d5f9a4e0542b75ee8240
Aleksey <ZhuravlyovAlex@mail.ru>
2022-06-22 19:12:25 +0300

    add class MyListTest

---
+++ MyCollections/src/test/java/com/zhuravlyov/MyListTest.java
@@ -0,0 +1,60 @@
+package com.zhuravlyov;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MyListTest {
+
+    private MyList<Integer> list;
+
+    @Before
+    public void setUp() throws Exception {
+
+//        list = new MyArrayList<Integer>();
+        for (int i = 0; i < 10; i++) {
+            list.add(i);
+        }
+    }
+
+    @Test
+    public void whenAddedNumber15InTheEndPositionThenSizeMustBeIncreased() {
+        assertTrue(list.add(15));
+        assertEquals(11, list.size());
+    }
+
+    @Test
+    public void whenAddedNumber777InMiddlePositionThenSizeMustBeIncreased() {
+        assertTrue(list.add(5, 777));
+        assertEquals(11, list.size());
+    }
+
+    @Test
+    public void whenRemovedNumberByIndexThenSizeMustBeDecreased() {
+        list.removeAt(5);
+        assertEquals(9, list.size());
+    }
+
+    @Test
+    public void whenRemovedNumberByNameThenSizeMustBeDecreased() {
+//        MyList<String> list2 = new MyArrayList<String>();
+//        for (int i = 0; i < 10; i++) {
+//            list.add("Brand" + i);
+//        }
+//        list.add(6, "BMW");
+//        list.remove("BMW");
+        assertEquals(10, list.size());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenTryingToCallNonExistentIndexThenThrownException() {
+        list.get(10);
+    }
+
+    @Test
+    public void whenClearingThenSizeMustBe0() {
+        list.clear();
+        assertEquals(0, list.size());
+    }
+}
